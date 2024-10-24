83e5afd2dbb305346cd3a00441d4f410a4d8b18c
MSLadanov <MSLadanov@gmail.com>
2024-10-24 09:19:18 +0300

    Базовый роутинг + страница ошибки

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import SignIn from './routes/SignIn/SignIn.tsx';
+import SignUp from './routes/SignUp/SignUp.tsx';
+import User from './routes/User/User.tsx';
+import Admin from './routes/Admin/Admin.tsx';
+import ErrorRoute from './routes/ErrorRoute/ErrorRoute.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <ErrorRoute />
+  },
+  {
+    path: "signin/",
+    element: <SignIn />,
+  },
+  {
+    path: "signup/",
+    element: <SignUp />,
+  },
+  {
+    path: 'user/',
+    element: <User />
+  },
+  {
+    path: 'admin/',
+    element: <Admin/>
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
