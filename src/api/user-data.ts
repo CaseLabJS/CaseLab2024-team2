e68eedfd23cfb7245faf4be131dc84ca966c3624
Sergey <murderer83@gmail.com>
2024-10-26 01:38:27 +0300

    feat: :sparkles: added requests for data user

---
+++ src/api/user-data.ts
@@ -0,0 +1,34 @@
+import { api } from "./api-config";
+
+type UserData = {
+  email: string;
+  display_name: string;
+  document_ids: number[];
+};
+
+// получить информацию о пользователе по email
+export const getUserData = async (email: string) => {
+  const response = await api.get<UserData>(`/users?email=${email}`);
+  return response.data;
+};
+
+// получить информацию о всех пользователях
+export const getAllUserData = async () => {
+  const response = await api.get<UserData[]>(`/users/all`);
+  return response.data;
+};
+
+// редактирование данных пользователя
+export const editUserData = async (email: string, display_name: string) => {
+  const response = await api.put<UserData>("/users", {
+    email,
+    display_name,
+  });
+  return response.data;
+};
+
+// удаление пользователя
+export const deleteUserData = async () => {
+  const response = await api.delete("/users");
+  return response.data;
+};
