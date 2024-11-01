f787ca17ff78444bc1c16570b5928f78db46d408
NadyaGus <ftotkexit@gmail.com>
2024-11-01 12:46:13 +0300

    feat: :sparkles: add user store

---
+++ src/entities/User/model/UserStore.ts
@@ -0,0 +1,104 @@
+import { registerUser } from '@/api/register-auth';
+import { deleteUserData, editUserData, getAllUserData, getUserData } from '@/api/req-user-data';
+import { makeAutoObservable, onBecomeObserved, runInAction } from 'mobx';
+
+import type { UserResponse } from './UserResponse';
+
+class UserStore {
+  users: UserResponse[];
+  email = '';
+  displayName = '';
+
+  constructor() {
+    makeAutoObservable(this);
+    this.users = [];
+
+    onBecomeObserved(this, 'users', () => {
+      this.getAllUsers().catch(console.error);
+    });
+
+    this.init();
+  }
+
+  async getAllUsers(): Promise<void> {
+    try {
+      const response = await getAllUserData();
+      runInAction(() => {
+        this.users = response;
+      });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  }
+
+  async createUser({ email, name, password }: { email: string; name: string; password: string }): Promise<void> {
+    try {
+      await registerUser({ email, display_name: name, password });
+      const user = await getUserData(email);
+      runInAction(() => {
+        this.users.push(user);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getUserData({ email }: { email: string }): Promise<void> {
+    try {
+      const response = await getUserData(email);
+      runInAction(() => {
+        this.email = response.email;
+        this.displayName = response.display_name;
+      });
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  }
+
+  async editUserData({
+    password,
+    display_name,
+    email,
+  }: {
+    password: string;
+    display_name: string;
+    email: string;
+  }): Promise<void> {
+    try {
+      const response = await editUserData({ password, display_name });
+      const user = this.users.find((user) => user.email === email);
+      runInAction(() => {
+        if (user) {
+          user.display_name = response.display_name;
+        }
+      });
+    } catch (error) {
+      console.error('Error editing user data:', error);
+    }
+  }
+
+  async deleteUser(): Promise<void> {
+    // TODO надо дождаться правок бэка, чтобы удалять можно было по почте
+    await deleteUserData();
+  }
+
+  async load(): Promise<void> {
+    try {
+      const user = await getUserData('admin@gmail.com');
+      // TODO надо продумать логику хранения мейла
+
+      runInAction(() => {
+        this.email = user.email;
+        this.displayName = user.display_name;
+      });
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  }
+
+  init(): void {
+    this.load().catch(console.error);
+  }
+}
+
+export { UserStore };
