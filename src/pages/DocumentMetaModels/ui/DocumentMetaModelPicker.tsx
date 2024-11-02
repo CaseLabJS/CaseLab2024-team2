37b95252bbfff7a62eb236010afc5498ddd75bff
imsur <imsurusov@gmail.com>
2024-11-02 21:22:00 +0300

    feat: :sparkles: use document filler

--- src/pages/DocumentMetaModels/ui/DocumentMetaModelPicker.tsx
+++ src/pages/DocumentMetaModels/ui/DocumentMetaModelPicker.tsx
@@ -4,9 +4,10 @@ import type { DocumentTypeResponse } from '@/entities/Document';
 import { useState, useEffect } from 'react';
 
 import { getAllDocTypes } from '@/api/req-doc-types.ts';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 interface DocumentMetaModelPickerProps {
-  setDocumentType: (type: string) => void;
+  setDocumentType: (type: DocumentTypeResponse | undefined) => void;
 }
 
 const DocumentMetaModelPicker = ({ setDocumentType }: DocumentMetaModelPickerProps): ReactElement => {
@@ -20,19 +21,30 @@ const DocumentMetaModelPicker = ({ setDocumentType }: DocumentMetaModelPickerPro
       .catch(console.log)
   }, [])
 
-  return <select onChange={event => setDocumentType(event.target.value)}>
-    <option value={''}>Документ не выбран</option>
-    {
-      documentMetaModels.map(document => {
-        return <option
-          key={document.id}
-          value={document.id}
-        >
-          {document.name}
-        </option>
-      })
-    }
-  </select>
+  return <div>
+    <FormControl sx={{ m: 1, minWidth: 256 }}>
+      <InputLabel id="document-type">Тип документа</InputLabel>
+      <Select
+        labelId="document-type-label"
+        id="document-type"
+        label="Документ"
+        variant={"outlined"}
+        onChange={event => setDocumentType(documentMetaModels.find(doc => doc.id == event.target.value as number))}
+      >
+        <MenuItem value=""><em>Не выбран</em></MenuItem>
+        {
+          documentMetaModels.map(document => {
+            return <MenuItem
+              key={document.id}
+              value={document.id}
+            >
+              {document.name}
+            </MenuItem>
+          })
+        }
+      </Select>
+    </FormControl>
+  </div>
 }
 
 export default DocumentMetaModelPicker;
--- src/pages/DocumentMetaModels/ui/DocumentMetaModels.tsx
+++ src/pages/DocumentMetaModels/ui/DocumentMetaModels.tsx
@@ -2,16 +2,19 @@ import type { ReactElement } from 'react';
 
 import { useState } from 'react';
 import DocumentMetaModelPicker from '@/pages/DocumentMetaModels/ui/DocumentMetaModelPicker.tsx';
+import DocumentFiller from '@/pages/DocumentMetaModels/ui/DocumentFiller.tsx';
+import { DocumentTypeResponse } from '@/entities/Document';
 
 
 const DocumentMetaModels = (): ReactElement => {
-  const [ documentType, setDocumentType ] = useState<string>("");
+  const [ documentType, setDocumentType ] = useState<DocumentTypeResponse | undefined>();
 
   console.log(documentType);
 
   return <div>
     Выберите документ для заполнения
     <DocumentMetaModelPicker  setDocumentType={setDocumentType}/>
+    <DocumentFiller documentType={documentType} />
   </div>
 }
 
