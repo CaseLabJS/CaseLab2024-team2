f6cbc20fd89e46656eba5299a85ab3775039d48f
Denis Tkachev <allpost-s@ya.ru>
2024-11-05 22:26:28 +0300

    feat: :construction: localStorage for notifications

--- src/shared/components/header/ui/Header.tsx
+++ src/shared/components/header/ui/Header.tsx
@@ -1,10 +1,10 @@
 import type { ReactElement } from "react";
 
-import { createElement, useState } from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import styles from './header.module.css';
 import { ROUTE_CONSTANTS } from '../../../../app/providers/router/config/constants';
+import styles from './header.module.css';
 import bellIcon from './images/bell.svg';
 import logo from './images/logo.svg';
 import chatIcon from './images/messenger.svg';
@@ -19,7 +19,7 @@ export const Header = (): ReactElement => {
     isAdmin: true,
   }
 
-  
+
 
   /**Пример уведомлений - должно приходить с backend (для разработки) */
   const notifications = [ // уведомления
@@ -27,7 +27,7 @@ export const Header = (): ReactElement => {
       id: 1,
       note: 'Уведомление',
       content: 'Все будет хорошо!',
-      isRead: true,
+      isRead: false,
     },
     {
       id: 2,
@@ -49,12 +49,11 @@ export const Header = (): ReactElement => {
     },
   ];
 
-  const isRead = notifications.filter((item) => item.isRead === false).map((notif) => notif.isRead); //создание массива непрочитанных уведомлений, для определения их количества
 
   /**Функционал выпадающего меню пользователя: */
   const [isOpenMenu, setOpenMenu] = useState(false);
   const handleOpenMenu = (): void => setOpenMenu(!isOpenMenu); // меню выпадает и исчезает по клику на иконку user
-  const closeMenu = ():void => { // меню исчезает если открывают другое меню
+  const closeMenu = (): void => { // меню исчезает если открывают другое меню
     if (isOpenMenu === true) {
       handleOpenMenu();
     }
@@ -63,7 +62,7 @@ export const Header = (): ReactElement => {
   /**Функционал выпадающего меню уведомлений */
   const [isOpenNotes, setOpenNotes] = useState(false);
   const handleOpenNotes = (): void => setOpenNotes(!isOpenNotes); // меню выпадает и исчезает по клику на иконку bell
-  const closeNote = ():void => { // меню исчезает если открывают другое меню
+  const closeNote = (): void => { // меню исчезает если открывают другое меню
     if (isOpenNotes === true) {
       handleOpenNotes();
     }
@@ -71,19 +70,52 @@ export const Header = (): ReactElement => {
 
   /**Функционал клика по уведомлениям */
   //TODOсделать чтобы при клике по уведомлению появлялся текст уведомления, а само уведомление становилось прочитанным, т.е. не жирным и изменялось количество непрочитанных сообщений у колокольчика
-  // let read = [...notifications];
+  let read = [...notifications];
+  const localStorageKey: string = 'notifications';
 
-  // const readEl = (id:number) => { // уведомление становится прочитанным
-  //     read = notifications.map((note) => {
-  //     if (note.id === id) {
-  //         return {...note, isRead: true};
-  //     } return note;
-  // })
-  //     return read;
-  // }
+  // положить данные в localStorage:
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const toLocalStorage = (data: []) => {
+    localStorage.setItem(localStorageKey, JSON.stringify(data));
+  }
 
-  // const [showNoteContent, setShowNoteContent ] = useState(false);
+  //получение данных из localStorage:
+  const fromLocalStorage = (): [] => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const localNotifications: [] = JSON.parse(localStorage.getItem(localStorageKey) || '[]');
+    return localNotifications;
+  }
+
+  //обновление localStorage
+  const updateLocalStorage = (id: number): void => {
+    const data: { id: number, note: string, content: string, isRead: boolean }[] = fromLocalStorage();
+    const modifiedData: [] = data.map((item) => {
+      if (item.id === id) {
+        return { ...item, isRead: true };
+      } return item;
+    }) as [];
+    toLocalStorage(modifiedData);
+  }
+
+  toLocalStorage(read);
+  // updateLocalStorage(2);
+
+  // console.log('>>', fromLocalStorage());
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const readEl = (id: number) => { // уведомление становится прочитанным
+    updateLocalStorage(id);
+    read = fromLocalStorage();
+  }
+
+  const isRead = read.filter((item) => item.isRead === false).map((notif) => notif.isRead); //создание массива непрочитанных уведомлений, для определения их количества
+
+  // const [showNoteContent, setShowNoteContent] = useState(false);
   // const handleshowNoteContent = (): void => setShowNoteContent(!showNoteContent); // Содержание уведомления при клике появляется и исчезает
+  // const showContent = (note, id:number):void => {
+
+  //   if (showNoteContent)
+  // }
 
   /**Массив пунктов выпадающего меню пользователя: */
   const itemsAdmin = [ // пункты меню в случае администратора
@@ -121,21 +153,27 @@ export const Header = (): ReactElement => {
           <img className={`${styles.userHeader__userIcon} ${styles.userHeader__userIconChat}`} src={chatIcon} alt="chatIcon" />
         </button>
         <button className={styles.userHeader__button}>
-          <img onClick={() => {handleOpenNotes(); closeMenu()}} className={`${styles.userHeader__userIcon} ${styles.userHeader__userIconBell}`} src={bellIcon} alt="bellIcon" />
+          <img onClick={() => { handleOpenNotes(); closeMenu() }} className={`${styles.userHeader__userIcon} ${styles.userHeader__userIconBell}`} src={bellIcon} alt="bellIcon" />
           {
             isRead.length > 0 &&
             (<p className={styles.userHeader__notifications}>{isRead.length}</p>)
           }
           {
             isOpenNotes && (<ul className={styles.userNotes}>
-              {notifications.map((item) => (
-                <li key={item.id} style={{ fontWeight: item.isRead === false ? 'bold' : 'normal' }} >{item.note}</li>
+              {read.map((item) => (
+                <li onClick={(event) => {
+                  readEl(Number(event.currentTarget.getAttribute('data-id'))); console.log(event.currentTarget.getAttribute('data-id'));
+                }} data-id={item.id} key={item.id} style={{ fontWeight: item.isRead === false ? 'bold' : 'normal' }} >{item.note}
+                  {/* {
+                    showNoteContent && (<p>{item.content}</p>)
+                  } */}
+                </li>
               ))}
             </ul>)
           }
         </button>
         <button className={styles.userHeader__button}>
-          <img onClick= {() => {handleOpenMenu(); closeNote()}} className={`${styles.userHeader__userIcon} ${styles.userHeader__userIcon_user}`} src={userIcon} alt="userIcon" />
+          <img onClick={() => { handleOpenMenu(); closeNote() }} className={`${styles.userHeader__userIcon} ${styles.userHeader__userIcon_user}`} src={userIcon} alt="userIcon" />
           {
             isOpenMenu && (<ul className={styles.userMenu}>
               {items.map((item, i) => (
