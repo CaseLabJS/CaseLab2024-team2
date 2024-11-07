272fc6205d58ff8c18ae25c3041eb94c17a3020b
NikitaSimakov <mishaaniskin@gmail.com>
2024-11-07 22:08:23 +0300

    feat: :lipstick: Basic styles: Fonts imported, CSS variables added, installed modern-normalize.
    Добавлены CSS переменные цветов и шрифтов, установлен пакет Modern-normalize, шрифты импортированы в index.html.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import '@/app/styles/index.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 import { App } from './app/App';
 
