55a48d253dbdf5a8f3a06c8da331e33ac82c9cdc
NadyaGus <ftotkexit@gmail.com>
2024-11-08 17:15:01 +0300

    refactor: :zap: change router constants, add ru titles

--- src/widgets/breadcrumbs/model/getRussianRoute.ts
+++ src/widgets/breadcrumbs/model/getRussianRoute.ts
@@ -1,21 +1,15 @@
 import { ROUTE_CONSTANTS } from '@/app/providers/router/config/constants';
 
 type Route = keyof typeof ROUTE_CONSTANTS;
-type Path = (typeof ROUTE_CONSTANTS)[Route];
+type Path = (typeof ROUTE_CONSTANTS)[Route]['path'];
+type Title = (typeof ROUTE_CONSTANTS)[Route]['ruTitle'];
 
-const routes = {
-  [ROUTE_CONSTANTS.ROOT]: 'Главная',
-  [ROUTE_CONSTANTS.SIGN_IN]: 'Авторизация',
-  [ROUTE_CONSTANTS.USER]: 'Пользователь',
-  [ROUTE_CONSTANTS.ADMIN]: 'Администратор',
-  [ROUTE_CONSTANTS.CREATE_ATTRIBUTE]: 'Создание атрибута',
-  [ROUTE_CONSTANTS.DOCUMENT_TYPES]: 'Типы документов',
-} as const;
-
-function getRussianRoute(path: Path): string | null {
-  if (routes[path]) {
-    return routes[path];
-  }
+function getRussianRoute(path: Path): Title | null {
+  Object.entries(ROUTE_CONSTANTS).find(([_key, value]) => {
+    if (value.path === path) {
+      return value.ruTitle;
+    }
+  });
 
   return null;
 }
