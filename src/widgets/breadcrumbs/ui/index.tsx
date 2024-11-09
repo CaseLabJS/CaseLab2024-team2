c72b803e6b65f97b224504ca0a243a9314764862
NadyaGus <ftotkexit@gmail.com>
2024-11-09 12:35:57 +0300

    feat: :lipstick: add styles for breadcrumds

--- src/widgets/breadcrumbs/ui/index.tsx
+++ src/widgets/breadcrumbs/ui/index.tsx
@@ -1,7 +1,10 @@
 import type { ReactElement } from 'react';
 
+import { ROUTE_CONSTANTS } from '@/app/providers/router/config/constants';
+import HouseIcon from '@mui/icons-material/House';
 import { Breadcrumbs as MuiBreadcrumbs, Typography } from '@mui/material';
-import { Link, useLocation } from 'react-router-dom';
+import { Link as MuiLink } from '@mui/material';
+import { useLocation } from 'react-router-dom';
 
 import { getRussianRoute } from '../model/getRussianRoute';
 
@@ -19,19 +22,25 @@ const Breadcrumbs = (): ReactElement => {
     return <></>;
   }
 
-  return (
-    <MuiBreadcrumbs aria-label="breadcrumb">
-      {ruRoutes.map((route, index) =>
-        index !== ruRoutes.length - 1 ? (
-          <Link key={index} to={routes[index]}>
-            {route}
-          </Link>
-        ) : (
-          <Typography key={index}>{route}</Typography>
-        ),
-      )}
-    </MuiBreadcrumbs>
-  );
+  const routesComponents = ruRoutes.map((route, index) => {
+    if (route === ROUTE_CONSTANTS.ROOT.ruTitle) {
+      return (
+        <MuiLink key={index} color="inherit" href={ROUTE_CONSTANTS.ROOT.path}>
+          <HouseIcon color="inherit" sx={{ mt: 0.5 }} />
+        </MuiLink>
+      );
+    } else if (index !== ruRoutes.length - 1) {
+      return (
+        <MuiLink key={index} color="inherit" href={routes[index]}>
+          {route}
+        </MuiLink>
+      );
+    } else {
+      return <Typography key={index}>{route}</Typography>;
+    }
+  });
+
+  return <MuiBreadcrumbs aria-label="breadcrumb">{routesComponents}</MuiBreadcrumbs>;
 };
 
 export { Breadcrumbs };
