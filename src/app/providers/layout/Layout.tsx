c741fe0d3bc8b28a8cdd1e440ce49f879bc8fd1d
Denis Tkachev <allpost-s@ya.ru>
2024-11-10 17:10:10 +0300

    refactor: :fire: remove not necessary code

--- src/app/providers/layout/Layout.tsx
+++ src/app/providers/layout/Layout.tsx
@@ -17,38 +17,3 @@ const Layout = () : ReactElement => {
 };
 
 export default Layout;
-
-const DevHeader = (): ReactElement => {
-  const location = useLocation();
-  return (
-    <div>
-      <h1>Хэдер {location.pathname.includes('/admin') ? 'администратора' : 'пользователя'}</h1>
-      {/* Для разработки */}
-      <div
-        style={{
-          backgroundColor: 'rgba(255, 205, 210, 0.8)',
-          borderRadius: '12px',
-          padding: '20px',
-        }}
-      >
-        <button onClick={() => devLogOut()}>Выйти</button>
-      </div>
-      {/* Для разработки */}
-      {devCheckIsAdmin() &&
-        (location.pathname === '/admin' ? (
-          <NavLink to={'/user'}>Панель пользователя</NavLink>
-        ) : (
-          <NavLink to={'/admin'}>Панель администратора</NavLink>
-        ))}
-        {location.pathname === '/admin' && <NavLink to={'/admin/create-attribute'}>Создать аттрибут</NavLink>}
-    </div>
-  );
-};
-
-const DevFooter = (): ReactElement => {
-  return (
-    <div>
-      <h1>Футер</h1>
-    </div>
-  );
-};
