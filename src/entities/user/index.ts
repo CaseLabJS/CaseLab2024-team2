c55c5aab74392848277da3947683c3497f76051d
NadyaGus <ftotkexit@gmail.com>
2024-11-11 23:10:14 +0300

    refactor: move all file one by one in user folder

--- src/entities/user/index.ts
+++
@@ -1,6 +0,0 @@
-export type * from './model/types/UserUpdateRequest';
-export type * from './model/types/UserResponse';
-export type * from './model/types/UserToDocumentResponse';
-export type * from './model/types/RegisterRequest';
-export type * from './model/types/AuthenticationRequest';
-export type * from './model/types/AuthenticationResponse';
