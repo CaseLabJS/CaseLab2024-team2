50061e54d94ef27298da3bee1a2711bcdf47b0e9
Denis Tkachev <allpost-s@ya.ru>
2024-11-11 19:32:27 +0300

    feat: :construction: ui create user form

--- src/widgets/createUser/ui/index.tsx
+++ src/widgets/createUser/ui/index.tsx
@@ -1,48 +1,49 @@
-import type { ReactElement } from "react";
+import type { ReactElement } from 'react';
 
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-export const createUser = (): ReactElement => {
-  const initialValues = {
-    user: "",
-    email: "",
-    password: "",
-  };
+import styles from './userForm.module.css';
 
-
-
-  const onSubmit = (values: unknown):void => {
-
-
-    alert(JSON.stringify(values, null, 2))
-  }
-
+export const CreateUserForm = (): ReactElement => {
   return (
-    <Formik {...{initialValues, onSubmit}}>
-      {
-        () => (
-          <Form className="baseForm" noValidate>
-            <Field
-              type="text"
-              id="user"
-              className="formField"
-              name="user"
-            />
-            <Field
-              type="email"
-              id="email"
-              className="email formField"
-              name="email"
-            />
-            <Field
-              type="password"
-              id="password"
-              className="password formField"
-              name="password"
-            />
-          </Form>
-        )
-      }
-    </Formik>
-  )
-}
+    <div className={styles.userForm}>
+      <p className={styles.userForm__title}>Создать пользователя</p>
+      <Formik
+        initialValues={{ user: '', email: '', password: '' }}
+        validationSchema={Yup.object({
+          user: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          email: Yup.string()
+            .email('Invalid email address')
+            .required('Required'),
+          password: Yup.string()
+            .min(8, 'Must be 8 characters or more')
+            .required('Required'),
+
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form className={styles.userForm__form}>
+          <div>
+            <Field name="user" type="text" placeholder='Имя пользователя' />
+            <ErrorMessage className={styles.userFrom__error} name="user" />
+
+            <Field name="email" type="email" placeholder='Email' />
+            <ErrorMessage className={styles.userFrom__error} name="email" />
+
+            <Field name="password" type="text" placeholder='Пароль' />
+            <ErrorMessage className={styles.userFrom__error} name="password" />
+          </div>
+          <button className={styles.userForm__button} type="submit">Создать</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
