9d62cae00d8475a9017fab6e76c0a8dd846dc260
Sergey <murderer83@gmail.com>
2024-11-12 00:31:01 +0300

    added index-attribute

---
+++ src/entities/Attribute/index.ts
@@ -0,0 +1,4 @@
+export type * from './model/types/AttributeRequest';
+export type * from './model/types/AttributeResponse';
+export type * from './model/types/AttributeValueRequest';
+export type * from './model/types/AttributeValueResponse';
