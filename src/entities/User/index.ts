e03dcaf0c57ff4b35b774e9433c94d9ca8fd20f5
Sergey <murderer83@gmail.com>
2024-11-12 00:55:48 +0300

    added index-user

---
+++ src/entities/User/index.ts
@@ -0,0 +1,6 @@
+export type * from './model/types/UserUpdateRequest';
+export type * from './model/types/UserResponse';
+export type * from './model/types/UserToDocumentResponse';
+export type * from './model/types/RegisterRequest';
+export type * from './model/types/AuthenticationRequest';
+export type * from './model/types/AuthenticationResponse';
