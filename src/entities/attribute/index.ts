a59965c7c8e49d9296674ed2e083611d5133d48c
Sergey <murderer83@gmail.com>
2024-11-12 06:01:45 +0300

    rename AttributeStore

---
+++ src/entities/attribute/index.ts
@@ -0,0 +1,5 @@
+export type * from './model/types/AttributeRequest';
+export type * from './model/types/AttributeResponse';
+export type * from './model/types/AttributeValueRequest';
+export type * from './model/types/AttributeValueResponse';
+export { attributesStore } from './model/store';
