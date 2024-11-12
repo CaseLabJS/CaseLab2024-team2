3d6e098880ca06f50395636d55a12a2d05f385ab
Sergey <murderer83@gmail.com>
2024-11-12 06:07:08 +0300

    rename signature

--- src/entities/documents/model/types/DocumentFacadeResponse.ts
+++ src/entities/documents/model/types/DocumentFacadeResponse.ts
@@ -1,6 +1,6 @@
 import type { DocumentResponse } from '@/entities/documents/model/types/DocumentResponse';
 import type { DocumentVersionResponse } from '@/entities/documents/model/types/DocumentVersionResponse';
-import type { SignatureResponse } from '@/entities/result/model/types/SignatureResponse';
+import type { SignatureResponse } from '@/entities/signature/model/types/SignatureResponse';
 export interface DocumentFacadeResponse {
   document: DocumentResponse;
   latest_version: DocumentVersionResponse;
