1530ba7629cadd07f3a37915a10a557ec14e8695
Sergey <murderer83@gmail.com>
2024-11-12 05:59:21 +0300

    delete index atr

--- src/entities/attribute/index.ts
+++
@@ -1,5 +0,0 @@
-export type * from './model/types/AttributeRequest';
-export type * from './model/types/AttributeResponse';
-export type * from './model/types/AttributeValueRequest';
-export type * from './model/types/AttributeValueResponse';
-export { attributesStore } from './model/attributesStore';
