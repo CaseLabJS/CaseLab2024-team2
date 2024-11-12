db7bf8fcef62d7eb4062a8d12ffb43ed4e64e99a
Denis Tkachev <allpost-s@ya.ru>
2024-11-12 17:03:11 +0300

    chore: :art: route path in header is updated

--- src/shared/components/header/ui/Header.tsx
+++ src/shared/components/header/ui/Header.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement } from "react";
 
 import { ROUTE_CONSTANTS } from '@/app/providers/router/config/constants';
-import { devCheckIsAdmin, devLogOut } from "@/shared/utils/dev/dev-utils";
+import { devCheckIsAdmin } from "@/shared/utils/dev/dev-utils";
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import { useState, useEffect } from "react";
@@ -106,18 +106,18 @@ export const Header = (): ReactElement => {
   const itemsAdmin = [ // пункты меню в случае администратора
     {
       name: 'Профиль',
-      link: ROUTE_CONSTANTS.ADMIN,
+      link: ROUTE_CONSTANTS.ADMIN.path,
     },
     {
       name: 'Admin menu',
-      link: ROUTE_CONSTANTS.ADMIN,
+      link: ROUTE_CONSTANTS.ADMIN.path,
     },
   ];
 
   const itemsUser = [ // пункты меню в случае простого пользователя
     {
       name: 'Профиль',
-      link: ROUTE_CONSTANTS.SIGN_IN,
+      link: ROUTE_CONSTANTS.SIGN_IN.path,
     }
   ];
 
@@ -131,7 +131,7 @@ export const Header = (): ReactElement => {
   return (
     <header className={styles.header}>
       <div className={styles.userHeader}>
-        <NavLink className={styles.userHeader__link} to={ROUTE_CONSTANTS.ROOT.ruTitle}>
+        <NavLink className={styles.userHeader__link} to={ROUTE_CONSTANTS.ROOT.path}>
           <img className={styles.userHeader__logo} src={logo} alt="userHeaderLogo" />
         </NavLink>
         <nav className={styles.userHeader__icons}>
