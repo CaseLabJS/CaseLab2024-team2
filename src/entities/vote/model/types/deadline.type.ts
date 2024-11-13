fc985c35f7478be587d4ce3e08bd265159b8fb56
Vadim Bychkov <by4ckoff.vadik@yandex.ru>
2024-11-13 17:12:20 +0300

    fix: deadline.type - delete discription

--- src/entities/vote/model/types/deadline.type.ts
+++ src/entities/vote/model/types/deadline.type.ts
@@ -1,24 +1,23 @@
 interface Duration {
-  seconds: number; // `integer($int64)` соответствует `number`
+  seconds: number;
   zero: boolean;
-  nano: number; // `integer($int32)` соответствует `number`
+  nano: number;
   negative: boolean;
   positive: boolean;
 }
 
 interface Unit {
   durationEstimated: boolean;
-  duration: Duration; // Используем интерфейс `Duration` для вложенной структуры
+  duration: Duration;
   timeBased: boolean;
   dateBased: boolean;
 }
 
 export interface Deadline {
-  description: string;
-  seconds: number; // `integer($int64)` соответствует `number`
+  seconds: number;
   zero: boolean;
-  nano: number; // `integer($int32)` соответствует `number`
+  nano: number;
   negative: boolean;
   positive: boolean;
-  units: Unit[]; // Массив объектов `Unit`
+  units: Unit[];
 }
