217e03c7c24de1c8af1f499c56f822e24f3f7755
imsur <imsurusov@gmail.com>
2024-11-18 19:17:39 +0300

    fix: :ambulance: new api

--- src/entities/documents/api/req-doc-types.ts
+++ src/entities/documents/api/req-doc-types.ts
@@ -1,7 +1,11 @@
+import type { PaginationRequest } from '@/shared/types/paginationRequest';
+
 import { api } from '@/shared/http';
+import { buildSearchParams } from '@/shared/utils/buildSearchParams';
 
 import type { DocumentTypeRequest } from '../model/types/documentTypeRequest.type';
 import type { DocumentTypeResponse } from '../model/types/documentTypeResponse.type';
+import type { DocumentTypesPageResponse } from '../model/types/documentTypesPageResponse.type';
 
 // получение типа документа по id
 export const getDocType = async (id: number): Promise<DocumentTypeResponse> => {
@@ -10,8 +14,10 @@ export const getDocType = async (id: number): Promise<DocumentTypeResponse> => {
 };
 
 // получение всех типов документов
-export const getAllDocTypes = async (): Promise<DocumentTypeResponse[]> => {
-  const response = await api.get<DocumentTypeResponse[]>('/document_types');
+export const getAllDocTypes = async (paginationRequest: PaginationRequest): Promise<DocumentTypesPageResponse> => {
+  const response = await api.get<DocumentTypesPageResponse>('/document_types', {
+    params: buildSearchParams(paginationRequest),
+  });
   return response.data;
 };
 
