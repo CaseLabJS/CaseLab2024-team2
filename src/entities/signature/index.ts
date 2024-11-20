a9f77e95a9b7bc6c97c8fc3b8fceb85490b85b95
NikitaSimakov <mishaaniskin@gmail.com>
2024-11-20 23:22:00 +0300

    feat: :sparkles: Added signatures store
    Добавлен новый метод getDocumentSignatures, добавлен signatureStore

--- src/entities/signature/index.ts
+++ src/entities/signature/index.ts
@@ -1,2 +1,3 @@
 export type * from './model/types/signatureResponse.type';
 export type * from './model/types/signatureCreateRequest.type';
+export { signaturesStore } from './model/store/signaturesStore';
