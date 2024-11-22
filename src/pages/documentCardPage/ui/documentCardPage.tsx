728f18823f6d47f1e9f083dbaa4c492339fdce95
NadyaGus <ftotkexit@gmail.com>
2024-11-22 19:04:54 +0300

    feat: :construction: add route to router and add request for document

--- src/pages/documentCardPage/ui/documentCardPage.tsx
+++ src/pages/documentCardPage/ui/documentCardPage.tsx
@@ -1,10 +1,26 @@
 import type { ReactElement } from 'react';
 
 import { Breadcrumbs } from '@/widgets/breadcrumbs';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { documentsStore } from '@/entities/documents';
+import { observer } from 'mobx-react-lite';
+import { useParams } from 'react-router-dom';
+import { Layout } from '@/shared/components/layout';
 
-const DocumentCardPage = (): ReactElement => {
+const DocumentCardPage = observer((): ReactElement => {
+  const id = useParams().id;
+
+  // if (documentsStore.currentDocument === null) {
+  //   return <Typography>Загрузка...</Typography>;
+  // }
+
+  // if (id === undefined) {
+  //   return <Typography>Документ не найден</Typography>;
+  // }
+
+  documentsStore.getDocumentById(Number(id));
+
   const rows = [
     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
@@ -17,14 +33,16 @@ const DocumentCardPage = (): ReactElement => {
     { field: 'firstName', headerName: 'First name', width: 130 },
     { field: 'lastName', headerName: 'Last name', width: 130 },
   ];
+
   return (
-    <>
+    <Layout>
       <Breadcrumbs />
       <Box sx={{ backgroundColor: 'white', padding: '20px' }}>
         <DataGrid columns={columns} rows={rows} hideFooter />
+        <Typography variant="h4">{documentsStore.currentDocument?.document.name}</Typography>
       </Box>
-    </>
+    </Layout>
   );
-};
+});
 
 export { DocumentCardPage };
