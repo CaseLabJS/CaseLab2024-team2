8bc337dfe8a2994889bc47026c2862488251daf6
Sergey <murderer83@gmail.com>
2024-11-26 03:23:49 +0300

    refactor: :recycle: modified tabs

--- src/app/providers/router/config/constants.ts
+++ src/app/providers/router/config/constants.ts
@@ -4,6 +4,7 @@ export const ROUTE_CONSTANTS = {
   USER: { path: '/user', ruTitle: 'Пользователь' },
   ADMIN: { path: '/admin', ruTitle: 'Администратор' },
   ATTRIBUTES: { path: '/attributes', ruTitle: 'Аттрибуты' },
+  USERS: { path: '/users', ruTitle: 'Пользователи' },
   DOCUMENT_TYPES: { path: '/document-types', ruTitle: 'Типы документов' },
   USER_DOCUMENTS: { path: '/documents', ruTitle: 'Документы пользователя' },
   DOCUMENT_CARD: { path: '/:documentId', ruTitle: 'Документ' },
