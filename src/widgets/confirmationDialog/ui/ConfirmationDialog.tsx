f2ec4ac1e5edc94036747bba2e02c1286634b37d
imsur <imsurusov@gmail.com>
2024-12-05 15:41:16 +0300

    perf: :zap: added submitAndClose function

--- src/widgets/confirmationDialog/ui/ConfirmationDialog.tsx
+++ src/widgets/confirmationDialog/ui/ConfirmationDialog.tsx
@@ -1,29 +1,34 @@
-import type { ReactElement } from "react";
+import type { ReactElement } from 'react';
 
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 
 interface ConfirmationDialogProps {
-  open: boolean
+  open: boolean;
   onClose: () => void;
   onSubmit: () => void;
   children: ReactElement;
 }
 
 const ConfirmationDialog = ({ open, onClose, onSubmit, children }: ConfirmationDialogProps): ReactElement => {
-  return <Dialog open={open} onClose={onClose}>
-    <DialogTitle>Подтвердите действие</DialogTitle>
-    <DialogContent>
-      {children}
-    </DialogContent>
-    <DialogActions sx={{ alignItems: "end" }}>
-      <Button onClick={onClose} variant="text">
-        Отменить
-      </Button>
-      <Button onClick={onSubmit} variant="contained" color="error">
-        Подтвердить
-      </Button>
-    </DialogActions>
-  </Dialog>;
-}
+  function submitAndClose(): void {
+    onSubmit();
+    onClose();
+  }
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Подтвердите действие</DialogTitle>
+      <DialogContent>{children}</DialogContent>
+      <DialogActions sx={{ alignItems: 'end', mr: 2, mb: 2 }}>
+        <Button onClick={onClose} variant="text">
+          Отменить
+        </Button>
+        <Button onClick={submitAndClose} variant='contained'>
+          Подтвердить
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default ConfirmationDialog;
